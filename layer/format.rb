494703344da3a7517e93cb819596dc17ea581855
schmidt <schmidt@a3cd3f6d-7000-446a-b07d-c8d7327e5bd4>
2007-07-23 19:39:43 +0000

    * it is now possible to specify a certain model a _the_ model that should be rendered in xml view
    git-svn-id: https://www.nach-vorne.de/svn/master/wiki@48 a3cd3f6d-7000-446a-b07d-c8d7327e5bd4

--- layer/format.rb
+++ layer/format.rb
@@ -1,27 +1,46 @@
+class ContextWiki::Controllers::Pages
+  module XMLMethods
+    def get(*a)
+      if a[1] == "versions"
+        class << yield(:receiver)
+          def model
+            @page.versions
+          end
+        end
+      end
+      yield(:next, *a)
+    end
+  end
+  register XMLMethods => ContextR::XmlRequestLayer
+end
 module ContextWiki::Base
   def render(m)
-   super 
+    super
   end
-
   module XMLMethods
-    def render(*args)
-      puts "Here we go in XML yeah"
+    def render(m)
       model = fetch_model(yield(:receiver))
-      yield(:receiver).instance_variable_get(:@headers)["Content-Type"] = "application/xml"
+      yield(:receiver).instance_variable_get(:@headers)["Content-Type"] = 
+        "application/xml"
       model.nil? ? "" : model.to_xml
     end
 
     def fetch_model(receiver)
-      instance_vars = receiver.instance_variables
-      controller_name = fetch_controller_name(receiver)
-      instance_vars.delete_if{ |var_name| 
-            var_name != "@#{controller_name.singularize}" and
-            var_name != "@#{controller_name}" }
-      model = instance_vars.collect{ |var_name|
-            receiver.instance_variable_get(var_name) 
-      }.select{ |var|
-            var.respond_to? :to_xml
-      }.first
+      if receiver.respond_to?(:model)
+        receiver.model
+      else
+        likely_model_name = fetch_controller_name(receiver)
+
+        instance_vars = receiver.instance_variables
+        instance_vars.delete_if{ |var_name| 
+              var_name != "@#{likely_model_name.singularize}" and
+              var_name != "@#{likely_model_name}" }
+        model = instance_vars.collect{ |var_name|
+              receiver.instance_variable_get(var_name) 
+        }.select{ |var|
+              var.respond_to? :to_xml
+        }.first
+      end
     end
 
     def fetch_controller_name(receiver)
